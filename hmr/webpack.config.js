3d2592e33ba0b7d0642d34d0f3f8b2503423a64f
lyk <1052927018@qq.com>
2022-05-10 04:01:17 +0800

    不使用htmlWebpackPlugin时热更新避免reload的措施

--- hmr/webpack.config.js
+++ hmr/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	entry: './index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.join(__dirname, './')
+		path: path.join(__dirname, './dist')
 	},
 	mode: 'development',
 	devServer: {
@@ -13,7 +13,7 @@ module.exports = {
 		allowedHosts: 'all', // 'auto' | 'all' [string]；该选项允许将允许访问开发服务器的服务列入白名单。
 		port: 8080, // 端口号
 		open: true, // 设置其为 true 告诉 dev-server 在服务器已经启动后打开默认浏览器。
-		static: './' // boolean string object [string, object]； 该配置项允许配置从目录提供静态文件的选项（默认是 'public' 文件夹）。禁用时 将其设置为 false 
+		static: './dist' // boolean string object [string, object]； 该配置项允许配置从目录提供静态文件的选项（默认是 'public' 文件夹）。禁用时 将其设置为 false 
 	},
 	plugins: [
 	],
